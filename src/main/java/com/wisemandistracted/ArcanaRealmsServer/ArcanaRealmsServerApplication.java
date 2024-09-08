5c609ac76f0092629106cdc64a8e1c6ab7e1d167
wise-man-distracted <magewelle@live.com>
2024-09-08 18:49:15 -0300

    spring boot

---
+++ src/main/java/com/wisemandistracted/ArcanaRealmsServer/ArcanaRealmsServerApplication.java
@@ -0,0 +1,13 @@
+package com.wisemandistracted.ArcanaRealmsServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcanaRealmsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcanaRealmsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wisemandistracted/ArcanaRealmsServer/ArcanaRealmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisemandistracted.ArcanaRealmsServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcanaRealmsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
